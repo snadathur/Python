35e7ea8491d2ea92676422ac09181fb62b9bcd2a
vikram <nadathurvikram@gmail.cm>
2016-06-11 13:34:55 -0700

    Sol
    Temporary solution with a small GUI

--- graphics_for_randomnumbergenerator.py
+++ graphics_for_randomnumbergenerator.py
@@ -1,17 +1,35 @@
-import tkinter,sys,os,turtle 
+#Imports modules needed
+import tkinter,random
 from tkinter import*
 tk=Tk()
-#Imports modules needed
+
+#Functions needed for program
 def create_button(button_text,button_command):
 	btn = Button(tk, text=button_text, command=button_command)
 	btn.pack()
-create_button("test",None)
+
+def minnum():
+    print('Enter the minimum value for the random number:')
+    global minval
+    minval=int(input())
+
+def maxnum():
+    print('Enter the maximum value for the random number:')
+    global maxval
+    maxval=int(input())
+
+def randomize():
+    randnum=random.randint(minval,maxval)
+    print(randnum)
+
+#Gets parameters and gives an into to the program
+print('Welcome to the random number generator with a simple GUI by Vikram Nadathur')
+minnum()
+maxnum()
+
 #Creates buttons
-create_button("Enter the number of random numbers you want",None)
-create_button("Randomize",None)
+create_button("Randomize",randomize)
 
 
 
-
-
-
+#   PROGRAM BY VIKRAM NADATHUR 6/11/16                                            
