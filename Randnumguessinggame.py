690036e43a559ff314e3788dc67aa706d6ce0055
vikram <nadathurvikram@gmail.cm>
2016-06-12 15:18:53 -0400

    Fixed
    Fixed commit errors

--- Randnumguessinggame.py
+++ Randnumguessinggame.py
@@ -44,29 +44,6 @@ def main():
 
 
 main()
-intro()
-
-#Ask user for parameters
-print("Please enter the minimum value for the random number")
-minval=int(input())
-print('Please enter the maximum value for the random number')
-maxval=int(input())
-message='The number was %s'
-randnum=random.randint(minval,maxval)
-
-while 1 == 1:
-    print('Guess the number. To give up type 99')
-    guess=int(input())
-    if guess == randnum:
-        print('You guessed the number correctly')
-    elif guess == 99:
-        print(message % randnum)
-        print('Play again another day')
-        quit()
-    else:
-        print('Guess the number')
-    guess=int(input())
-
 
 #   PROGRAM BY VIKRAM NADATHUR 6/11/16  
 
