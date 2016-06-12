6ef8325180627c1151ccf1d8fad420c5ad44a5b8
vikram <nadathurvikram@gmail.cm>
2016-06-12 08:43:13 -0700

    A simple word scrambler
    Needs to be updated to ask user if they want to rescramble word

---
+++ WordScamblerGame.py
@@ -0,0 +1,15 @@
+from random import shuffle
+
+print('Welcome the Word Scrambler by Vikram Nadathur.\nEnter a word to scramble or type exit to leave')
+word=input()    
+
+if word == 'exit':
+    print('Have a good day')
+    quit()
+else:
+            word=list(word)
+            print(word)
+            print('The scrambled word is ')
+            shuffle(word)
+            print(word)
+
